88156a5e8ab1fb5ae26aa77ff96e1ecb51bffdfa
Paweł <pawel@ksienie.com>
2025-07-09 17:21:27 +0200

    All my plotting.

--- arche.c
+++ arche.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include "const.h"
-#include "dimage.h"
-#include "ksienie.h"
+#include "image.h"
+#include "model.h"
 #include "dandelion.h"
 
 int main(void){
@@ -12,94 +12,20 @@ int main(void){
     int q = 32;     // number of quants (angles)
     
     // Reserve memory for image
-    unsigned char *img = dimage(W,H);
+    unsigned char *image = dimage(W,H);
     
-    // Reserve memory for point location
-    int n = q * q;  // number of derivatives
-    double *seed = array(2, q);
-    double *expansion = array(2,n);
-    double *reduction = array(2,n);
-
-    /* Seed loop [circular] */
-    for (int i = 0 ; i < q ; i++){
-        double val = i * (2*PI)/q;
-
-        double vs = sin(val);
-        double vc = cos(val);
-
-        seed[i] = vs;
-        seed[i+q] = vc;
-    }
-
-    /* Expansion loop */
-    for (int i = 0 ; i < q ; i++){
-        for (int j = 0 ; j < q ; j++){
-            double ax = seed[i];
-            double ay = seed[i+q];
-
-            double bx = seed[j];
-            double by = seed[j+q];
-
-            double x = 0.;
-            double y = 0.;
-            
-            expansion[i+j*q] = ax + bx;
-            expansion[i+j*q+n] = ay + by;
-            
-            reduction[i+j*q] = ax * bx;
-            reduction[i+j*q+n] = ay * by;
-        }
-    }
+    // Calculate
+    double *seed = circle(q);
+    double *expansion = expand(seed, q);
+    double *reduction = reduce(seed, q);
     
-    /* plotting */
-    // Example line
-    int *_line = line(0, 0, 3, 1);
-    for (int i = 1 ; i < _line[0] + 1 ; i++){
-        img[ravel(_line[i], _line[i+_line[0]], 2)] = 255;
-    }
-    
-    // Plot [seed]
-    // Iterate points
-    for (int i = 0 ; i < q ; i++) {
-        double vs = seed[i];
-        double vc = seed[i+q];
+    // Plot
+    plot(image, seed, q);
+    plot(image, expansion, q*q);
+    plot(image, reduction, q*q);
 
-        int px = dx(vs);
-        int py = dy(vc);
-
-        img[ravel(px,py,0)] = 255;
-        img[ravel(px,py,1)] = 255;
-        img[ravel(px,py,2)] = 255;
-    }
-
-    for (int i = 0 ; i < n ; i++) {
-        double vs = 0;
-        double vc = 0;
-        int px = 0;
-        int py = 0;
-
-        vs = expansion[i];
-        vc = expansion[i+n];
-
-        px = dx(vs);
-        py = dy(vc);
-
-        img[ravel(px,py,0)] = 255;
-        img[ravel(px,py,1)] = 255;
-        // img[ravel(px,py,2)] = 255;
-
-        vs = reduction[i];
-        vc = reduction[i+n];
-
-        px = dx(vs);
-        py = dy(vc);
-
-        img[ravel(px,py,0)] = 255;
-        // img[ravel(px,py,1)] = 255;
-        img[ravel(px,py,2)] = 255;
-    }
-
-    bmp(img, W, H, "foo.bmp");
+    // Store
+    bmp(image, W, H, "foo.bmp");
 
     return 0;
 }
--- const.h
+++ const.h
@@ -2,5 +2,5 @@
 // Processing constants
 //
 
-const float PI = 3.1416, M = 2.125;
-const int W = 1024, H = 1024;
+const float PI = 3.1415, M = 2;
+const int W = 512, H = 512;
