10e4b699e872158ae91fe012a752a66708de0a58
Paweł <pawel@ksienie.com>
2025-07-09 13:53:06 +0200

    Automated preview + module decomposition.

--- arche.c
+++ arche.c
@@ -2,78 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "const.h"
+#include "dimage.h"
 #include "ksienie.h"
+#include "dandelion.h"
 
-/* Constants */
-const float PI = 3.1416;
-const int W = 256, H = 256;
-const float M = 2.125;
-
-/* Digital image mapping */
-float xd(int px){ return (2*M*px/(W-1))-M; }
-float yd(int py){ return (2*M*py/(H-1))-M; }
-
-int dx(float x){ return ((x+M)/(2*M))*(W-1); }
-int dy(float y){ return ((y+M)/(2*M))*(H-1); }
-
-int ravel(int x, int y, int c) { return (x+y*W)*3+c; } // ravel
-
-/* Metric function */
-double metric(float x, float y, double p){
-    switch ((int) p)
-    {
-    case 1:
-        return _abs(x) + _abs(y);
-    case 2:
-        return sqrt(x*x + y*y);
-    default:
-        return pow((pow(_abs(x), p) + pow(_abs(y), p)), 1/p);
-    }
-}
-
-/* Business logic */
-int is_circle(int px, int py, float cx, float cy, float radius){
-    float x = xd(px) - cx;
-    float y = yd(py) - cy;
-
-    float distance = metric(x, y, 2);
-
-    float lw = .01;
-    int verify = distance < radius;
-
-    return verify*255;
-}
-
-int *line(float ax, float ay, float bx, float by) {
-    int dax = dx(ax), day = dy(ay);
-    int dbx = dx(bx), dby = dy(by);
-
-    int mx = _iabs(dax - dbx);
-    int my = _iabs(day - dby);
-
-    int n_points = mx > my ? mx : my;
-
-    int *_line = NULL;
-    _line = (int *)malloc(sizeof(int) * (n_points * 2 + 1));
-    _line[0] = n_points;
-
-    for (int i = 0 ; i < n_points ; i++){
-        _line[i+1] = dx(ax+(i*_abs(ax-bx)/n_points));
-        _line[i+1+n_points] = dy(ay+(i*_abs(ay-by)/n_points));
-    }
-
-    return _line;
-}
-
-/* main() function */
 int main(void){
+    // Variables
     int q = 32;     // number of quants (angles)
-    int n = q * q;  // number of derivatives
-
+    
     // Reserve memory for image
     unsigned char *img = dimage(W,H);
     
     // Reserve memory for point location
+    int n = q * q;  // number of derivatives
     double *seed = array(2, q);
     double *expansion = array(2,n);
     double *reduction = array(2,n);
@@ -111,18 +53,13 @@ int main(void){
     
     /* plotting */
     // Example line
-    int *_line = line(0, 0, 1, .5);
-
-    printf("%i !!!\n", _line[0]);
-    
-    // Plot seed
+    int *_line = line(0, 0, 3, 1);
     for (int i = 1 ; i < _line[0] + 1 ; i++){
-        int px = _line[i];
-        int py = _line[i+_line[0]];
-        img[ravel(px, py, 2)] = 255;
+        img[ravel(_line[i], _line[i+_line[0]], 2)] = 255;
     }
-
-
+    
+    // Plot [seed]
+    // Iterate points
     for (int i = 0 ; i < q ; i++) {
         double vs = seed[i];
         double vc = seed[i+q];
@@ -162,7 +99,7 @@ int main(void){
         img[ravel(px,py,2)] = 255;
     }
 
-    bmp(img, W, H);
+    bmp(img, W, H, "foo.bmp");
 
     return 0;
 }
---
+++ dimage.h
@@ -0,0 +1,66 @@
+//
+// Digital Image Mapping
+//
+
+/* Directional conversion */
+float xd(int px){ return (2*M*px/(W-1))-M; }
+float yd(int py){ return (2*M*py/(H-1))-M; }
+
+int dx(float x){ return ((x+M)/(2*M))*(W-1); }
+int dy(float y){ return ((y+M)/(2*M))*(H-1); }
+
+/* Circular addresation */
+int ravel(int x, int y, int c) { return (x+y*W)*3+c; }
+
+/* Image generation */
+unsigned char *dimage(int width, int height){
+    unsigned char *_dimage = NULL;
+    _dimage = (unsigned char *)malloc(sizeof(double)*width*height*3);
+    memset(_dimage,0.,width*height*3);
+    return _dimage;
+}
+
+void bmp(unsigned char * image, int w, int h, char *filename){
+    FILE *f;
+    
+    int filesize = 54 + 3 * w * h;
+
+    unsigned char bmpfileheader[14] = { 'B','M',
+         0, 0, 0, 0, 
+         0, 0, 0, 0,
+        54, 0, 0, 0
+    };
+    unsigned char bmpinfoheader[40] = {
+        40, 0, 0, 0, 
+         0, 0, 0, 0, 
+         0, 0, 0, 0, 
+         1, 0,24, 0
+    };
+    unsigned char bmppad[3] = {0,0,0};
+
+    bmpfileheader[ 2] = (unsigned char)(filesize    );
+    bmpfileheader[ 3] = (unsigned char)(filesize>> 8);
+    bmpfileheader[ 4] = (unsigned char)(filesize>>16);
+    bmpfileheader[ 5] = (unsigned char)(filesize>>24);
+
+    bmpinfoheader[ 4] = (unsigned char)(       w    );
+    bmpinfoheader[ 5] = (unsigned char)(       w>> 8);
+    bmpinfoheader[ 6] = (unsigned char)(       w>>16);
+    bmpinfoheader[ 7] = (unsigned char)(       w>>24);
+    bmpinfoheader[ 8] = (unsigned char)(       h    );
+    bmpinfoheader[ 9] = (unsigned char)(       h>> 8);
+    bmpinfoheader[10] = (unsigned char)(       h>>16);
+    bmpinfoheader[11] = (unsigned char)(       h>>24);
+
+    f = fopen(filename,"wb");
+    fwrite(bmpfileheader,1,14,f);
+    fwrite(bmpinfoheader,1,40,f);
+    for(int i=0; i<h; i++)
+    {
+        fwrite(image+(w*(h-i-1)*3),3,w,f);
+        fwrite(bmppad,1,(4-(w*3)%4)%4,f);
+    }
+
+    free(image);
+    fclose(f);
+}
