2e24ead337ae81ede8f50c6d37246b1b9691f401
Paweł <pawel@ksienie.com>
2025-07-11 11:28:03 +0200

    Demo.

--- const.h
+++ const.h
@@ -2,9 +2,9 @@
 // Processing constants
 //
 
-float S = .05;
-float PI = 3.1415;
-// float PI = 2137;
-const float M = 3;
+float S = 0;
+// float PI = 3.1416;
+float PI = 2137;
+const float M = 5;
 const int W = 1024, H = 1024;
-const int BUFFER_SIZE = 512;
+const int BUFFER_SIZE = 2048;
--- generator.py
+++ generator.py
@@ -1,12 +1,12 @@
 import numpy as np
 np.set_printoptions(suppress=True, precision=True)
 
-q = 512;
-l = 30972;
+q = 10000;
+l = 32;
 ls = np.linspace(-np.pi*l,l*np.pi,q)
 A = np.sin(ls)
 
-A = np.random.uniform(size=q)
+# A = np.random.uniform(size=q)
 
 for a in A:
     print('%.8f' % a)
